6da0980df1d5d8c1ba55fb14dbfe65ea9497146f
Omar1Sorour <omarmas0510@gmail.com>
2025-03-11 02:34:12 +0200

    Update Admin.java

--- SchoolManagementSystem/src/Admin.java
+++ SchoolManagementSystem/src/Admin.java
@@ -2,17 +2,6 @@ import java.util.List;
 
 public class Admin extends Staff {
     
-    // Constructor
-    public Admin(int staffID, String name, String email, String role, StaffStatus status) {
-        this.staffID = staffID;
-        this.name = name;
-        this.email = email;
-        this.role = role;
-        this.status = status;
-        this.managedModules = managedModules;
-    }
-
-    
     public String viewStatistics() {
         
         return "Statistics displayed";
